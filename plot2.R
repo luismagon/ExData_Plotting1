3dda0f4ab8e93dec65cd1401b2f088bd92e893da
luismagon <luismateogonz@gmail.com>
2020-03-25 20:34:17 +0100

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,16 @@
+# First you should download and unzip in your workspace the file from 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+doc <- read.table("./Data/household_power_consumption.txt", header = TRUE,
+                  sep = ";", nrows = 70000, na.strings = "?", stringsAsFactors = FALSE)
+data <- doc[doc$Date == "1/2/2007" | doc$Date == "2/2/2007", ]
+datetime <- paste(data$Date, data$Time)
+data$Date_time <- strptime(datetime, format = "%d/%m/%Y %H:%M:%S")
+
+# Open the device
+png(filename = "./Figures/plot2.png", width = 480, height = 480, units = "px")
+
+plot(data$Date_time, data$Global_active_power, type = "l", 
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+
+dev.off()
